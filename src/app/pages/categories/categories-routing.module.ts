85399046e6915c3c2dc30d5e2b28f84db738a57c
Cleuton Barbosa <barcleuton@gmail.com>
2020-01-19 21:45:47 -0200

    adding categories module

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: CategoryListComponent},
+  {path:'new', component: CategoryFormComponent},
+  {path:':id/edit', component: CategoryFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
     CategoriesRoutingModule
