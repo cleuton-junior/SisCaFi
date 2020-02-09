224f5f4b5227b27065b7d9480fa8906fe09f08c9
Cleuton Barbosa <barcleuton@gmail.com>
2020-02-08 22:16:12 -0200

    improving category-list-component template

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CategoryListComponent } from './category-list/category-list.component';
-import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoryListComponent } from "./category-list/category-list.component";
+import { CategoryFormComponent } from "./category-form/category-form.component";
 
 const routes: Routes = [
   {path:'', component: CategoryListComponent},
