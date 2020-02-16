ee44504b982f34e02b5b8139f684cbe7d4f842f5
Cleuton Barbosa <barcleuton@gmail.com>
2020-02-16 20:23:48 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,14 @@
+import{InMemoryDbService} from "angular-in-memory-web-api";
+
+export class InMemoryDataBase implements InMemoryDbService{
+     createDb(){
+       const categories=[
+           {id: 1, name: 'Moradia', description: 'Pagamento de Contas de Casa'},
+           {id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios'},
+           {id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc.'},
+           {id: 4, name: 'Salário', description: 'Recebimento de Salário'},
+           {id: 5, name: 'Freelas', description: 'Trabalhos com freelancer'},
+       ];
+       return {categaries}
+     }
+}
