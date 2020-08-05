bb844de37efe0a97b5ede41a15804bf4a545ec5e
Cleuton Barbosa <barcleuton@gmail.com>
2020-08-05 17:38:16 -0300

    adding Reports base

---
+++ src/app/pages/reports/reports.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { ReportsRoutingModule } from './reports-routing.module';
+import { ReportsComponent } from './reports/reports.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@NgModule({
+  declarations: [ReportsComponent],
+  imports: [
+    SharedModule,
+    ReportsRoutingModule
+  ]
+})
+export class ReportsModule { }
