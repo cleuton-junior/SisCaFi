f0ca1446fd8d1a133f423348291c08a24bb5b0d2
Cleuton <cbjunior@latam.stefanini.com>
2020-08-24 23:26:45 -0300

    Improving reports

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,10 +1,12 @@
 import { Injectable,Injector } from '@angular/core';
 
 import { Observable} from "rxjs";
-import { flatMap, catchError} from "rxjs/operators";
+import { flatMap, catchError, map} from "rxjs/operators";
 import { Entry } from "./entry.model";
 import { CategoryService } from '../../categories/shared/category.service';
 import { BaseResourceService } from 'src/app/shared/services/base-resources.services';
+
+import * as moment from 'moment'
 
 @Injectable({
   providedIn: 'root'
@@ -23,6 +25,13 @@ export class EntryService extends BaseResourceService<Entry> {
     return this.setCategoryAndSendToServer(entry,super.update.bind(this))  
   }
 
+  getByMonthAndYear(month: number, year:number):Observable<Entry[]>{
+    // case api -this.http.get("api/entries?month&year").subscribe(entries)
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries,month, year))
+    )
+  }
+
   private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry>{
     //settings performed to test without api
     return this.categoryService.getById(entry.categoryId).pipe(
@@ -32,5 +41,15 @@ export class EntryService extends BaseResourceService<Entry> {
       }),
       catchError(this.handleError)
     )
+  }
+
+  private filterByMonthAndYear(entries: Entry[],month: number, year: number){
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date,"DD/MM/YYYY");
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
+
+      if(monthMatches && yearMatches) return entry;
+    })
   }
 }
--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsComponent } from './reports/reports.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ChartModule} from 'primeng/chart';
 
 @NgModule({
   declarations: [ReportsComponent],
   imports: [
+    ChartModule,
     SharedModule,
-    ReportsRoutingModule
+    ReportsRoutingModule  
   ]
 })
 export class ReportsModule { }
